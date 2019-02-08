da2003eb7d0a9f6245db68710091dfc69e14f259
Eli Hurst <Hurste04@gmail.com>
2019-02-07 20:39:42 -0600

    Added physical button

--- components/src/pages/index.js
+++ components/src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import {Address, Button} from "../components/Element"
 
 const IndexPage = () => (
   <Layout>
@@ -14,6 +15,10 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <Address>
+      My Address is 123 Sleepy Hollow Road.  
+    </Address>
+    <Button variant="primary"> Contact Me </Button>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
