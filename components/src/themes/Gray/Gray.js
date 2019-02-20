279844c4a5c4ac06f6a563a5921322715d45e8f6
Eli Hurst <Hurste04@gmail.com>
2019-02-19 22:17:29 -0600

    Created IconButton component

--- components/src/themes/Gray/Gray.js
+++ components/src/themes/Gray/Gray.js
@@ -1,73 +1,94 @@
-const Gray = { ...theme, defaults, variants }
-export { Gray }
-
 const theme = {
-    space: [0, 4, 8, 16, 32, 64, 129, 256, 512],
+    space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
     lineHeights: {
-        solid: 1,
-        title: 1.25,
-        copy: 1.5
+      solid: 1,
+      title: 1.25,
+      copy: 1.5
     },
     letterSpacings: {
-        normal: 'normal',
-        tracked: '0.1em',
-        tight: '-0.05em',
-        mega: '0.25em'
+      normal: 'normal',
+      tracked: '0.1em',
+      tight: '-0.05em',
+      mega: '0.25em'
     },
     borders: [
-        0,
-        '1px solid',
-        '2px solid',
-        '3px solid',
-        '4px solid',
-        '8px solid',
-        '16px solid',
-        '32px solid',
+      0,
+      '1px solid',
+      '2px solid',
+      '3px solid',
+      '4px solid',
+      '8px solid',
+      '16px solid',
+      '32px solid'
     ],
     colors: {
-        primary: {
-            light: 'hsl(208, 13%, 35%)',
-            main: 'hsl(208, 13%, 25%)',
-            dark: 'hsl(208, 13#, 25%)',
-            contrastText: '#ffffff'
-        },
-        text: {
-            primary: 'hsl(208, 13%, 15%)',
-            secondary: 'hsl(208, 13%, 45%)',
-            disabled: 'hsl(208, 13%, 75%)',
-            hint: '(208, 13%, 75%)'
-        }
+      primary: {
+        light: 'hsl(208, 13%, 35%)',
+        main: 'hsl(208, 13%, 25%)',
+        dark: 'hsl(208, 13%, 15%)',
+        contrastText: '#ffffff'
+      },
+      text: {
+        primary: 'hsl(208, 13%, 15%)',
+        secondary: 'hsl(208, 13%, 45%)',
+        disabled: 'hsl(208, 13%, 75%)',
+        hint: 'hsl(208, 13%, 75%)'
+      }
     }
-}
-
-const defaults = {
+  }
+  
+  const defaults = {
     button: {
-        padding: `${theme.space[2] / 16}em ${(theme.space[3] + 4) /16}em`,
-        border: theme.borders[3],
-    textTransform: 'uppercase',
-letterSpacing: theme.letterSpacings.tracked    }
-}
-
-const variants = {
-   button: {
-       primary: {
-           ...defaults.button,
-           color: theme.colors.primary.main,
-           borderColor: theme.colors.primary.main
-       },
-       contrast: {
-           ...defaults.button,
-           color: theme.colors.primary.contrastText,
-           borderColor: theme.colors.primary.contrastText
-       }
-   },
-   linkButton: {
-       primary: {
-           color: theme.colors.primary.main
-       },
-       contrast: {
-           color: theme.colors.primary.contrastText
-       }
-   },
-
-}
+      padding: `${theme.space[2] / 16}em ${(theme.space[3] + 4) / 16}em`,
+      border: theme.borders[3],
+      textTransform: 'uppercase',
+      letterSpacing: theme.letterSpacings.tracked
+    },
+    text: {
+      lineHeight: theme.lineHeights.copy
+    }
+  }
+  
+  const variants = {
+    button: {
+      primary: {
+        ...defaults.button,
+        color: theme.colors.primary.main,
+        borderColor: theme.colors.primary.main
+      },
+      contrast: {
+        ...defaults.button,
+        color: theme.colors.primary.contrastText,
+        borderColor: theme.colors.primary.contrastText
+      }
+    },
+    iconButton: {
+      primary: {
+        color: theme.colors.primary.main
+      },
+      contrast: {
+        color: theme.colors.primary.contrastText
+      }
+    },
+    linkButton: {
+      primary: {
+        color: theme.colors.primary.main
+      },
+      contrast: {
+        color: theme.colors.primary.contrastText
+      }
+    },
+    text: {
+      primary: {
+        ...defaults.text,
+        color: theme.colors.text.primary
+      },
+      contrast: {
+        ...defaults.text,
+        color: theme.colors.primary.contrastText
+      }
+    }
+  }
+  
+  const Gray = { ...theme, defaults, variants }
+  export { Gray }
