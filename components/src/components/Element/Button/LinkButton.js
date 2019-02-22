f28c0d34912e032a5efcdcb3f07b48ac6cc2be2a
Eli Hurst <Hurste04@gmail.com>
2019-02-21 18:51:45 -0600

    reorganized gatsby

--- components/src/components/Element/Button/LinkButton.js
+++ components/src/components/Element/Button/LinkButton.js
@@ -3,13 +3,12 @@ import PropTypes from 'prop-types'
 import { ButtonBase } from './ButtonBase'
 import styled from 'styled-components'
 
-
 const StyledButton = styled(ButtonBase)`
-${props => props.theme.variants.LinkButton[props.variant || 'primary']};
+  ${props => props.theme.variants.linkButton[props.variant || 'primary']};
 `
 
 export const LinkButton = props => <StyledButton {...props} />
 
-LinkButton.PropTypes = {
-    variant: PropTypes.string
+LinkButton.propTypes = {
+  variant: PropTypes.string
 }
